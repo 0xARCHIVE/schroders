04f3eb5ac814f815f74295dd4e123873f380af0c
Steven Burnett <texruska@users.noreply.github.com>
2022-02-19 19:01:05 +0000

    Linting

--- tests/keypad/test_get_knight_moves.py
+++ tests/keypad/test_get_knight_moves.py
@@ -11,21 +11,40 @@ class TestGetKnightMoves(unittest.TestCase):
     def test_invalid_start_coord(self):
         """Input a start_coord not in the keypad."""
         keypad = {}
-        start_coord = (0,0)
+        start_coord = (0, 0)
 
         self.assertEqual(get_knight_moves(keypad, start_coord), set())
 
     def test_valid_start_coord(self):
         """Input start_coord within the keypad."""
-        keypad = {(0,0)}
-        start_coord = (0,0)
+        keypad = {(0, 0)}
+        start_coord = (0, 0)
 
         self.assertEqual(get_knight_moves(keypad, start_coord), set())
 
     def test_valid_moves(self):
         """Input a start_coord with moves within the keypad."""
-        keypad = {(0,0), (2,1), (2,-1), (-2,1), (-2,-1), (1,2), (-1,2), (1,-2), (-1,-2)}
-        start_coord = (0,0)
-        expected = {(2,1), (2,-1), (-2,1), (-2,-1), (1,2), (-1,2), (1,-2), (-1,-2)}
+        keypad = {
+            (0, 0),
+            (2, 1),
+            (2, -1),
+            (-2, 1),
+            (-2, -1),
+            (1, 2),
+            (-1, 2),
+            (1, -2),
+            (-1, -2),
+        }
+        start_coord = (0, 0)
+        expected = {
+            (2, 1),
+            (2, -1),
+            (-2, 1),
+            (-2, -1),
+            (1, 2),
+            (-1, 2),
+            (1, -2),
+            (-1, -2),
+        }
 
         self.assertEqual(get_knight_moves(keypad, start_coord), expected)
